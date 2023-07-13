34dd87a12925b2862c9c137b781b50703d6bc49c
RajKKapadia <raajforyou@gmail.com>
2023-07-13 13:33:37 +0530

    added function to create messages

--- quran_chatgpt/helper/utils.py
+++ quran_chatgpt/helper/utils.py
@@ -1,3 +1,6 @@
+from langchain.schema import HumanMessage, SystemMessage
+
+
 def get_context(data: list) -> list:
 
     context = ''
@@ -7,3 +10,30 @@ def get_context(data: list) -> list:
         context += f'{data[i]["response"]}\n'
 
     return context
+
+
+def generate_messages(messages: list, query: str, name: str) -> list:
+    formated_messages = [SystemMessage(
+        content=f"""Consider yourself a highly conversational Muslim scholar and refer to Tafsir Ibn Kathir or the Quran in your answer and please go into detail and ask questions back given context. Refer to the user as {name} in the beggining of the answer.""")]
+    for m in messages:
+        formated_messages.append(
+            HumanMessage(content=m['query'])
+        )
+        formated_messages.append(
+            SystemMessage(content=m['response'])
+        )
+    formated_messages.append(HumanMessage(content=query))
+    return formated_messages
+
+
+def create_string_chunks(string, length):
+    words = string.split()
+    sentences = []
+    temp_string = ''
+    for w in words:
+        if len(temp_string) > length:
+            sentences.append(f'{temp_string}...')
+            temp_string = ''
+        temp_string += f'{w} '
+    sentences.append(temp_string)
+    return sentences
